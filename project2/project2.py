55d80118624790ea4a7d3cefcff98b9218e070f1
jacobkang24 <148184732+jacobkang24@users.noreply.github.com>
2025-02-12 00:26:33 -0800

    Update project2.py

--- project2/project2.py
+++ project2/project2.py
@@ -1,12 +1,3 @@
-# project2.py
-#
-# ICS 33 Spring 2024
-# Project 2: Learning to Fly
-#
-# This is the main module that runs the entire program.
-#
-# YOU WILL NOT NEED TO MODIFY THIS FILE AT ALL
-
 from p2app import EventBus
 from p2app import Engine
 from p2app import MainView
